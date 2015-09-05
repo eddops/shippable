d2252445b6ce2ac382736fed39c0d3edd1c49825
edwinship <eddie.sammie@gmail.com>
2015-09-05 18:26:11 +0530

    Added new file

---
+++ ner.py
@@ -0,0 +1,17 @@
+"""
+Name Entity Recognition Module.
+"""
+import sys
+from textblob import TextBlob
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+filetoopen = open('filename.txt', 'r')
+
+lines = filetoopen.readlines()
+
+for line in lines:
+    line = line.encode('utf-8')
+    blob = TextBlob(line)
+    phrases = blob.noun_phrases
+    print str(phrases)
